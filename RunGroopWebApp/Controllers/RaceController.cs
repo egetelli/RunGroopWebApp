0ede1ff1ddbf1d94f4a1b649130154e1c2ceb028
egetelli <egetelli@hotmail.com>
2024-01-09 15:13:31 +0300

    Controllers and their views are done

--- RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
 
 namespace RunGroopWebApp.Controllers
 {
+
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Race race = _context.Races.Include(a => a.Address).FirstOrDefault(r => r.Id == id);
+
+            return View(race);
         }
     }
 }

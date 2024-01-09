007c59f5bf228b006eabdf1d9a6f985eda3af334
egetelli <egetelli@hotmail.com>
2024-01-09 13:46:33 +0300

    Adding Controllers

---
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        public ClubController(ApplicationDbContext context)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

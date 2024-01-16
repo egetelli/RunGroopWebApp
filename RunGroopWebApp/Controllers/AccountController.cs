fb08e4675c3b0f9589951726e8e680baf04d41c0
egetelli <egetelli@hotmail.com>
2024-01-16 13:41:58 +0300

    Adding identity.entityframeworkcore to project and add migration for database, also seed the users in that table

---
+++ RunGroopWebApp/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM login)
+        {
+
+        }
+    }
+}

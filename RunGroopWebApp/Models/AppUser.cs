e9b14314835ec7114448926f9aeb0eb3a42e8570
egetelli <egetelli@hotmail.com>
2024-02-12 12:25:41 +0300

    account controller

--- RunGroopWebApp/Models/AppUser.cs
+++ RunGroopWebApp/Models/AppUser.cs
@@ -10,8 +10,8 @@ namespace RunGroopWebApp.Models
         public int? Pace { get; set; }
         public int? Milage { get; set; }
         [ForeignKey("Address")]
-        public int AddressId { get; set; }
-        public Address Address { get; set; }
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
         public ICollection<Club> Clubs { get; set; }
         public ICollection<Race> Races { get; set; }
 

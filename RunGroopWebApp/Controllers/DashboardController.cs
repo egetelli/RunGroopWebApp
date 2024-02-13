9d01f2e1757fca6742f1a5376980b8c456ca1b89
egetelli <egetelli@hotmail.com>
2024-02-13 11:55:36 +0300

    Adding dashboard pt1

---
+++ RunGroopWebApp/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context, IUserService)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
